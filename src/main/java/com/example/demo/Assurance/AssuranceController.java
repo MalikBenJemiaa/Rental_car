ec98739fcedd9bc18e5c7b87e70329b197956aee
azyzhssin <hssine.medaziz1@gmail.com>
2023-10-10 17:22:13 +0100

    assurance package is done

--- src/main/java/com/example/demo/Assurance/AssuranceController.java
+++ src/main/java/com/example/demo/Assurance/AssuranceController.java
@@ -1,15 +1,40 @@
 package com.example.demo.Assurance;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/Assurance")
 public class AssuranceController {
 private final AssuranceService assuranceService;
+
 @Autowired
 public AssuranceController(AssuranceService assuranceService) {
         this.assuranceService = assuranceService;
+    }
+    @GetMapping
+    public List<Assurance> getAllAuthors() {
+        return assuranceService.getAllAssurance();
+    }
+    @GetMapping("/{id}")
+    public Optional<Assurance> getAuthorById(@PathVariable Long id) {
+        return assuranceService.getAssuranceById(id);
+    }
+    @PostMapping
+    public Assurance createAuthor(@RequestBody Assurance assurance) {
+        return assuranceService.createAssurance(assurance);
+    }
+
+    @PutMapping("/{id}")
+    public Assurance updateAuthor(@PathVariable Long id, @RequestBody Assurance updatedAssurance) {
+        return assuranceService.updateAssurance(id, updatedAssurance);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteAuthor(@PathVariable Long id) {
+        assuranceService.deleteAssurance(id);
     }
 }
--- src/main/java/com/example/demo/Assurance/AssuranceService.java
+++ src/main/java/com/example/demo/Assurance/AssuranceService.java
@@ -1,10 +1,11 @@
 package com.example.demo.Assurance;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
-
+@Service
 public class AssuranceService {
     private AssuranceRepo assuranceRepo;
     public AssuranceService(){

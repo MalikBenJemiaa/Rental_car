e621fb26aeb53e29e16518c5cf4462d1341768a9
azyzhssin <hssine.medaziz1@gmail.com>
2023-10-31 17:53:13 +0100

    pushing backup before pulling

--- src/main/java/com/example/demo/cars/CarsController.java
+++ src/main/java/com/example/demo/cars/CarsController.java
@@ -43,6 +43,7 @@ public class CarsController {
     @DeleteMapping("/deleteCars/{id}")
     public void deleteCars(@PathVariable Long id) {
         carsService.deleteCars(id);
+        //deleted
     }
 
 }

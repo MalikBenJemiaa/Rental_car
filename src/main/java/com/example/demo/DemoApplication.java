b9281be2bfea0fe1d1ca5ad841218ebbadb73d03
MalikBenJemiaa <melekbenjemiaa05@gmail.com>
2023-11-04 17:56:46 +0100

    the admin account in created when we build the project

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,5 +1,8 @@
 package com.example.demo;
 
+import com.example.demo.Accounts.AccountsRepo;
+import com.example.demo.DbInitializr.InitializrService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -13,8 +16,12 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin
 public class DemoApplication {
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+
+		InitializrService initializrService = null;
+		initializrService.createTheFirstAdmin();
 	}
 @GetMapping("/")
 	public String hello(){
